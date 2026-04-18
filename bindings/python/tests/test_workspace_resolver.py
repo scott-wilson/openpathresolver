534c3ff23ba546486b9d672087f92e200da101d5
Scott Wilson <scott@propersquid.com>
2026-04-17 21:32:26 -0700

    fix(python): Have the metadata fields return the right type

--- bindings/python/tests/test_workspace_resolver.py
+++ bindings/python/tests/test_workspace_resolver.py
@@ -3,12 +3,15 @@ from __future__ import annotations
 import asyncio
 from typing import TYPE_CHECKING
 
+import hypothesis
 import pytest
+from hypothesis import strategies
 
 import openpathresolver
 
 if TYPE_CHECKING:
     import collections.abc
+    import typing
 
 
 @pytest.mark.asyncio
@@ -250,3 +253,71 @@ def test_create_workspace_regression_segfault(
         )
 
     asyncio.run(main())
+
+
+@hypothesis.given(
+    metadata=strategies.dictionaries(
+        strategies.text(),
+        strategies.recursive(
+            strategies.none()
+            | strategies.booleans()
+            | strategies.integers(min_value=-1000, max_value=1000)
+            | strategies.floats(allow_nan=False, allow_infinity=False),
+            extend=lambda children: (
+                strategies.lists(children)
+                | strategies.dictionaries(strategies.text(), children)
+            ),
+        ),
+    )
+)
+@pytest.mark.asyncio
+async def test_create_workspace_regression_wrong_metadata_types(
+    metadata: dict[str, typing.Any],
+) -> None:
+    config = openpathresolver.Config(
+        {
+            "int": openpathresolver.IntegerResolver(3),
+            "str": openpathresolver.StringResolver(r"\w+"),
+        },
+        [
+            openpathresolver.PathItem(
+                "path",
+                "",
+                None,
+                openpathresolver.Permission.Inherit,
+                openpathresolver.Owner.Inherit,
+                openpathresolver.PathType.Directory,
+                deferred=False,
+                metadata=metadata,
+            ),
+        ],
+    )
+
+    def validate(a: typing.Any, b: typing.Any) -> None:  # noqa: ANN401
+        assert type(a) is type(b)
+
+        if a is None or isinstance(a, (bool, int, float, str)):
+            assert a == b
+        elif isinstance(a, list):
+            assert len(a) == len(b)
+
+            for item_a, item_b in zip(a, b):
+                validate(item_a, item_b)
+        elif isinstance(a, dict):
+            assert len(a) == len(b)
+            assert set(a) == set(b)  # Validate the keys
+
+            for key, value_a in a.items():
+                value_b = b[key]
+                validate(value_a, value_b)
+        else:
+            raise NotImplementedError
+
+    async def io_function(
+        config: openpathresolver.Config,  # noqa: ARG001
+        template_args: collections.abc.Mapping[str, openpathresolver.TemplateValue],  # noqa: ARG001
+        resolved_path_item: openpathresolver.ResolvedPathItem,
+    ) -> None:
+        validate(metadata, resolved_path_item.metadata())
+
+    await openpathresolver.create_workspace(config, {}, {}, io_function)
